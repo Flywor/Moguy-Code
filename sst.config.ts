cbc40d30923636d4abe769229a5dbf97c227484a
jiantao-luo <jiantao-luo@apple.com>
2026-06-17 00:00:34 +0800

    chore: remove enterprise and stats packages

--- sst.config.ts
+++ sst.config.ts
@@ -31,16 +31,13 @@ export default $config({
     const stage = await import("./infra/stage.js")
     await import("./infra/app.js")
     const lake = stage.deployAws ? await import("./infra/lake.js") : undefined
-    const stats = stage.deployAws ? await import("./infra/stats.js") : undefined
     const { stat } = await import("./infra/console.js")
-    await import("./infra/enterprise.js")
     if ($app.stage === "production" || $app.stage === "vimtor") {
       await import("./infra/monitoring.js")
     }
 
     return {
       StatWorkerUrl: stat.url,
-      ...(stats ? { StatsUrl: stats.app.url } : {}),
       ...(lake
         ? {
             LakeUrl: lake.lakeIngest.properties.url,
